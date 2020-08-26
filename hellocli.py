a82643c6bb20224207f41ad500aa4ed075571a9e
Nate Thompson <haplo@haplolabs.io>
2020-08-26 14:51:33 +0000

    Updated files

---
+++ hellocli.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import click
+from hello import marco
+
+@click.command()
+@click.option('--name')
+def callmarco(name):
+    result = marco(name)
+    click.echo(click.style(f'{result}', bg='blue', fg='white'))
+
+if __name__ == '__main__':
+    #pylint: disable
+
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,12 @@
 from hello import marco
-
+from click.testing import CliRunner
+from hellocli import callmarco
 
 def test_marco():
     assert "Polo" == marco("Marco")
+
+def test_search():
+    runner = CliRunner()
+    result = runner.invoke(callmarco, ['--name', 'Marcos'])
+    assert result.ext_code == 0
+    assert 'No!' in result.output
